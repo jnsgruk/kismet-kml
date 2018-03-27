1873ad94aa05dacce34f31e15578e523f1b58c3b
Jon Seager <jonseager@gmail.com>
2018-03-27 16:57:45 +0100

    Seperated the clients/aps/other in the KML file and populated extended data for each including clinet macs and probed ssids

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import json
 
 class KMLGen():
 
-  def __init__(self, inputFile, outputFile):
+  def __init__(self, inputFile, outputFile, jsonFile):
     self.inputFile = inputFile
     self.outputFile = outputFile
+    self.jsonFile = jsonFile
 
     self.debug = True 
     
@@ -22,10 +23,20 @@ class KMLGen():
 
     self.getData()
     self.parseData()
+   
+    self.createKML()
+
+    jsonOut = {
+      "clients": self.clients,
+      "aps": self.aps,
+      "bridged": self.bridged,
+      "other": self.other
+    }
+
+    output = open(self.jsonFile, "w")
+    output.write(json.dumps(jsonOut))
+    output.close()
     
-
-    # self.createKML()
-
   def getData(self):
     conn = sqlite3.connect(self.inputFile)
     conn.row_factory = sqlite3.Row
@@ -117,35 +128,87 @@ class KMLGen():
       document = KML.kml(KML.Document())
 
       # Create an icon style for each Placemark to use
-      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('50B41E14'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="blue_icon"), id="blue_circle"))
-      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('ff00C2ff'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="amber_icon"), id="amber_circle"))
-      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('ff00ff00'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="green_icon"), id="green_circle"))
+      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('50B41E14'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="blue_circle"), id="blue_circle"))
+      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('ff00C2ff'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="amber_circle"), id="amber_circle"))
+      document.Document.append(KML.Style(KML.IconStyle(KML.scale(1.0), KML.color('ff00ff00'), KML.Icon(KML.href("http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"),), id="green_circle"), id="green_circle"))
 
-      for row in self.rows:
-        fields = {}
-        device_json = json.loads(row["device"])
-        
-        fields["Common Name"] = device_json["kismet.device.base.commonname"]
-        fields["Channel"] = device_json["kismet.device.base.channel"]
-
-        # IF TYPE = Wi-Fi AP
-        #  SSID: device_json["dot11.device"][""dot11.device.last_beaconed_ssid""]
-
-        style = "#blue_circle" if row["type"] == "Wi-Fi Client" else "#green_circle"
-
+      for client in self.clients:
+        style = "#blue_circle"
         pm = KML.Placemark(
-          KML.name(common_name),
+          KML.name(client["Common Name"]),
           KML.styleUrl(style),
-          KML.Point(KML.coordinates(row["avg_lon"]/100000,",",row["avg_lat"]/100000)),
-          KML.ExtendedData(
-            KML.Data(KML.value(row["devmac"], name="Device MAC"))
-            # 
-            # 
-            # LOOP OVER ATTRIBUTES HERE
-            # 
-            # 
-          )
+          KML.Point(KML.coordinates(client["Average Longitude"],",",client["Average Latitude"])),
         )
+        
+        extData = KML.ExtendedData(
+          KML.Data(KML.value(client["Device MAC"], name="Device MAC")),
+          KML.Data(KML.value(client["First Seen"], name="First Seen")),
+          KML.Data(KML.value(client["Last Seen"], name="Last Seen")),
+          KML.Data(KML.value(client["Channel"], name="Channel"))
+        )
+          
+        for probe in client["Probes"]:
+          extData.append(KML.Data(KML.value(probe["SSID"], name="Probed SSID")))
+
+        pm.append(extData)
+        document.Document.append(pm)
+
+      for ap in self.aps:
+        style = "#green_circle"
+        pm = KML.Placemark(
+          KML.name(ap["Common Name"]),
+          KML.styleUrl(style),
+          KML.Point(KML.coordinates(ap["Average Longitude"],",",ap["Average Latitude"])),
+        )
+        
+        extData = KML.ExtendedData(
+          KML.Data(KML.value(ap["Device MAC"], name="Device MAC")),
+          KML.Data(KML.value(ap["First Seen"], name="First Seen")),
+          KML.Data(KML.value(ap["Last Seen"], name="Last Seen")),
+          KML.Data(KML.value(ap["Channel"], name="Channel")),
+          KML.Data(KML.value(ap["SSID"], name="SSID"))
+        )
+          
+        for client in ap["Clients"]:
+          extData.append(KML.Data(KML.value(client["Device MAC"], name="Client Device MAC")))
+
+        pm.append(extData)
+        document.Document.append(pm)
+
+      for other in self.other:
+        style = "#amber_circle"
+        pm = KML.Placemark(
+          KML.name(other["Common Name"]),
+          KML.styleUrl(style),
+          KML.Point(KML.coordinates(other["Average Longitude"],",",other["Average Latitude"])),
+        )
+        
+        extData = KML.ExtendedData(
+          KML.Data(KML.value(other["Device MAC"], name="Device MAC")),
+          KML.Data(KML.value(other["First Seen"], name="First Seen")),
+          KML.Data(KML.value(other["Last Seen"], name="Last Seen")),
+          KML.Data(KML.value(other["Channel"], name="Channel"))
+        )
+
+        pm.append(extData)
+        document.Document.append(pm)
+
+      for bridged in self.bridged:
+        style = "#amber_circle"
+        pm = KML.Placemark(
+          KML.name(bridged["Common Name"]),
+          KML.styleUrl(style),
+          KML.Point(KML.coordinates(bridged["Average Longitude"],",",bridged["Average Latitude"])),
+        )
+        
+        extData = KML.ExtendedData(
+          KML.Data(KML.value(bridged["Device MAC"], name="Device MAC")),
+          KML.Data(KML.value(bridged["First Seen"], name="First Seen")),
+          KML.Data(KML.value(bridged["Last Seen"], name="Last Seen")),
+          KML.Data(KML.value(bridged["Channel"], name="Channel"))
+        )
+
+        pm.append(extData)
         document.Document.append(pm)
 
       output = open(self.outputFile, "w")
@@ -161,10 +224,11 @@ class KMLGen():
 parser = argparse.ArgumentParser(description="Generates a KML file from the output file of Kismet (2017 Development Version")
 
 parser.add_argument('--input-file', '-i', required=True, type=str, help="Path to Kismet file. Usually a *.kismet file.")
-parser.add_argument('--output-file', '-o', required=True, type=str, help="Path to desired KML output file")
+parser.add_argument('--output-kml-file', '-k', required=True, type=str, help="Path to desired KML output file")
+parser.add_argument('--output-json-file', '-j', required=True, type=str, help="Path to desired JSON output file")
 args = parser.parse_args()
 
 # Create a new instance of the Wigle class
-kmlGen = KMLGen(args.input_file, args.output_file)
+kmlGen = KMLGen(args.input_file, args.output_kml_file, args.output_json_file)
 
 
